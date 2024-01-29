b180c8ce180209514c81959a66e5bc2e255f1184
Daecius Teo <s10241810@connect.np.edu.sg>
2024-01-30 01:40:24 +0800

    find the max increase in list

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -3,3 +3,13 @@ def cash_on_hand_insights(file_name: str, output_file_path: str):
         next(file)
         data_list = [line.strip().split(",") for line in file]
     value_list = [int(parts[1]) for parts in data_list]
+    #read from file into the list.
+    
+    with open(output_file_path, "a") as output_file:
+        if sorted(value_list, reverse=False) == value_list:
+            max_increase = None
+            for i in range(1, len(value_list)):
+                increase = value_list[i] - value_list[i - 1]
+                if max_increase is None or increase > max_increase:
+                    max_increase = increase
+                    #find the max increase in list.
