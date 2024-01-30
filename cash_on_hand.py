4000f949bfb9d5077ab87abefb690bf8449627ce
Daecius Teo <s10241810@connect.np.edu.sg>
2024-01-31 01:40:36 +0800

    code for completed COH, left with writting into file for output

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -12,6 +12,7 @@ def cash_on_hand_insights(file_name: str, output_file_path: str):
             max_increase = None
             for i in range(1, len(value_list)):
                 increase = value_list[i] - value_list[i - 1]
+                # [i] is to find the differnece in value inbetween the values within the list
                 if max_increase is None or increase > max_increase:
                     max_increase = increase
                     #find the max increase in list.
@@ -21,6 +22,7 @@ def cash_on_hand_insights(file_name: str, output_file_path: str):
                 #to check if the values within the value list is sorted in a descending order
                 max_decrease_index = None
                 max_decrease = None
+                # none is used as a variable to define
                 for i in range(1, len(value_list)):
                      decrease = value_list[i] - value_list[i - 1]
                      #This line calculates the difference between the current and previous cash surplus values.
@@ -30,6 +32,22 @@ def cash_on_hand_insights(file_name: str, output_file_path: str):
                           max_decrease_index = i
                 output_file.write("[CASH DEFICIT] CASH ON EACH DAY IS LOWER THAN THE PREVIOUS DAY\n")
                 output_file.write(f"[HIGHEST CASH DEFICIT] DAY: {data_list[max_decrease_index][0]}, AMPUNT: SGD {abs(max_decrease)}\n")
+        else:  #if not in descending order.
+             decrease = None
+             max_decrease = None
+             decrease_list = [] #all decrease data will be in this list
+             deficit_data = [] 
+             deficit_days = set() #days deficit happened
+             for i in range(1, len(value_list)):
+                  decrease= value_list[i] - value_list[i - 1]
+                  if max_decrease is None or decrease< max_decrease:
+                       if decrease < 0:
+                            decrease_list.append(decrease) #add all decrease values into this list)
+                            deficit_days.add(data_list[i][0]) #to get the number of day whr decreased happened
+                            deficit_data.append((data_list[i][0], decrease)) #appending the day corresponding deficit values into list
+
+
+
 
                                   
 
