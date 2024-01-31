76d002d898380424a29c2723eb854f87e527084e
Khin <yukiieipa@gmail.com>
2024-01-31 11:57:57 +0800

    analysis of net profit data for highest increase in net profit

--- profit_loss.py
+++ profit_loss.py
@@ -16,6 +16,14 @@ def profit_loss_insights(
             max_increase = None
             for i in range(1, len(value_list)):
                 increase= value_list[i] - value_list[i-1]
-    # caluculate the difference between everyday value 
+        # caluculate the difference between everyday value 
+                if max_increase is None or increase > max_increase:
+                    max_increase = increase 
+                output_file.write ("[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN PREVIOUS DAY\n")
+                output_file.write (f"[HIGHEST NET PROFIT SURPLUS] DAY: {data_list[i][0]}, Amount: SGD{max_increase}\n")
+        #analyze net profit data and keeps track of the highest increase in net profit 
+        #contains information about the net profit surplus to an output file, including the day with the highest surplus and the corresponding amount
+        
+
 
     
