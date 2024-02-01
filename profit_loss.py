2efb3e027ea6eaac86796f853d90df3b510e9dbb
Veron <s10259455@connect.np.edu.sg>
2024-02-01 15:40:38 +0800

    Included command

--- profit_loss.py
+++ profit_loss.py
@@ -5,26 +5,33 @@ def profit_loss_insights(
         file_name: str,
         output_file_path:str,
 ):
-    with open(file_name, 'r') as file:
+#Read data from the input file
+    with open(file_name, 'r') as file:  
         data_list = []
-        next (file)
+        next (file) 
         for line in file:
             data = line.strip().split(",")
             data_list.append(data)
+#Extract net profit values and store them in a list
     value_list = [int (parts[4]) for parts in data_list] #to keep the net profit info in value list
+#Open the output file in append mode
     with open (output_file_path, "a") as output_file:
+#Check if the net profit values are sorted in ascending order
         if sorted(value_list, reverse= False) == value_list:
+#Find the highest net profit surplus
             max_increase = None
             for i in range(1, len(value_list)):
                 increase= value_list[i] - value_list[i-1] # caluculate/find the difference between everyday value 
         
                 if max_increase is None or increase > max_increase:
-                    max_increase = increase 
+                    max_increase = increase
+#Write the highest net profit surplus information to the output file
                 output_file.write ("[NET PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN PREVIOUS DAY\n")
                 output_file.write (f"[HIGHEST NET PROFIT SURPLUS] DAY: {data_list[i][0]}, Amount: SGD{max_increase}\n")
         #analyze net profit data and keeps track of the highest increase in net profit 
         #contains information about the net profit surplus to an output file, including the day with the highest surplus and the corresponding amount
         elif sorted(value_list, reverse = True) == value_list:
+#Find the highest net profit deficit
             max_decrease_index = None
             max_decrease = None
             for i in range(1, len(value_list)):
@@ -32,9 +39,10 @@ def profit_loss_insights(
                 if max_decrease is None or decrease < max_decrease:
                     max_decrease = decrease
                     max_decrease_index = i
+ #Write the highest net profit deficit information to the output file
                 output_file.write("[NET PROFIT DEFICIT] NET PROFIT ON EACH DAY IS LOWER THAN PREVIOUS DAY\n")
         else:
-
+#Find the top 3 net profit deficits
             decrease = None
             max_decrease = None
             decrease_list = []
